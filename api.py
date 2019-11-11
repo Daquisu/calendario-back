2bacfcf3cc58c921fb9baaf9640e4d862846b68d
Vinicius Imaizumi <50253469+Daquisu@users.noreply.github.com>
2019-11-11 10:01:50 -0300

    Add files via upload

--- api.py
+++ api.py
@@ -6,22 +6,25 @@ from flask import Flask
 def same_year_and_month(day1, day2):
     return day1[:7] == day2[:7]
 
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
 app = Flask(__name__)
 
-@app.route('/')
-
-@app.route('/download_month')
 
 @app.route('/download_month/<year_and_month>')
 def download_month(year_and_month):
-    return 'Oi'
     zipname = year_and_month + '.zip'
-#    with zipfile.ZipFile(zipname, 'w') as zipf:
-    zipf = zipfile.ZipFile(year_and_month + '.zip', 'w')
-    for folder in os.listdir('./best'):
-       if same_year_and_month(folder, year_and_month):
-           zipf.write(folder)
-    zipf.close()
+    with zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED) as zipf:
+#    zipf = zipfile.ZipFile(year_and_month + '.zip', 'w')
+        for hashtag_label in os.listdir('./best'):
+            for folder in os.listdir('./best/' + hashtag_label):
+                if same_year_and_month(folder, year_and_month):
+                    zipdir('./best/' + hashtag_label + '/' + folder, zipf)
+#    zipf.close()
     return send_file(zipname,
                      mimetype = 'zip',
                      attachment_filename = zipname,
