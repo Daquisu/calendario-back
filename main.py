c4eb63274f438d125d7c652b238b38e6e2b498cd
Vinytt <38005211+Vinytt@users.noreply.github.com>
2020-08-27 07:22:59 -0300

    Update main.py
    Co-authored-by: gustapoll <59609311+gustapoll@users.noreply.github.com>

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ def custom_data(post):
 def zip_non_used_files():
     for hashtag in HASHTAG_LABELS:
         path = f"./#{hashtag}"
-        arqZip = zipfile.ZipFile("#" + hashtag + "ZIP.zip", "w", zipfile.ZIP_DEFLATED)
+        arqZip = zipfile.ZipFile(f"#{hashtag}ZIP.zip", "w", zipfile.ZIP_DEFLATED)
         for file in os.listdir(path):
             arqZip.write(os.path.join(path, str(file)), str(file))
             print("Zipando " + str(file))
@@ -199,4 +199,3 @@ if __name__ == '__main__':
     sstart_cron()
 
 # download_hashtags_last_7_days(hashtag_labels)
-
